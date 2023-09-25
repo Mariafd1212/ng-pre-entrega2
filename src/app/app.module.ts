9b86467c13b0df17f548b862de4cbc9f197dd13c
Maria Flores <mariafd1212@gmail.com>
2023-09-25 02:54:06 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ModoNocturnoComponent } from './modo-nocturno/modo-nocturno.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModoNocturnoComponent,
+    FooterComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.scss'],
+})
+export class ListaComponent {
+  title = 'Lista de estudiates';
+}
